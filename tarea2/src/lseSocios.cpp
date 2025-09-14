4917f52ace6569d4ddb4b277412384677b51f9ce
NicoPagVic <nico.pagvic2@gmail.com>
2025-09-14 13:12:15 -0300

    [M] LSESocio y Prestamo Completados

--- tarea2/src/lseSocios.cpp
+++ tarea2/src/lseSocios.cpp
@@ -2,40 +2,115 @@
 #include "../include/lseSocios.h"
 
 struct rep_lseSocios {
+	TSocio socio;
+	rep_lseSocios* sig;
 };
 
-TLSESocios crearTLSESociosVacia(){
-    return NULL;
+TLSESocios crearTLSESociosVacia(TSocio socio){
+	TLSESocios nuevoLseSocios = new rep_lseSocios;
+	nuevoLseSocios->socio = socio;
+	nuevoLseSocios->sig = NULL;
+	return nuevoLseSocios;
 }
 
 bool esVaciaTLSESocios(TLSESocios lseSocios){
+	if (lseSocios == NULL){
+		return true;
+	}
 	return false;
 }
 
 void imprimirTLSESocios(TLSESocios lseSocios){
+	printf("Lista de Socios:\n");
+	while (lseSocios != NULL){
+		imprimirTSocio(lseSocios->socio);
+		lseSocios = lseSocios->sig;
+	}
 }
 
 void liberarTLSESocios(TLSESocios &lseSocios){
+	while (lseSocios != NULL){
+		TLSESocios aux = lseSocios;
+		lseSocios = lseSocios->sig;
+		liberarTSocio(aux->socio);
+		delete aux;
+	}
+	lseSocios = NULL;
+}
+
+void insTLSESocios(TLSESocios &lseSocios, TSocio socio){
+	TLSESocios nuevoNodo = new rep_lseSocios;
+	nuevoNodo->socio = socio;
+	nuevoNodo->sig = lseSocios;
+	lseSocios = nuevoNodo;
 }
 
 void insertarTLSESocios(TLSESocios &lseSocios, TSocio socio){
+	if (lseSocios == NULL || fechaAltaTSocio(socio) < fechaAltaTSocio(lseSocios->socio)){
+		insTLSESocios(lseSocios, socio);
+	} else {
+		TLSESocios actual = lseSocios;
+		while(actual->sig != NULL && fechaAltaTSocio(socio) >= fechaAltaTSocio(actual->sig->socio)){
+			actual = actual->sig;
+		}
+		insTLSESocios(actual->sig, socio);
+	}
 }
 
 bool existeSocioTLSESocios(TLSESocios lseSocios, int ci){
+	while (lseSocios != NULL){
+		if (ci == ciTSocio(lseSocios->socio)){
+			return true;
+		}
+	}
 	return false;
 }
 
 TSocio obtenerSocioTLSESocios(TLSESocios lseSocios, int ci){
+	while (lseSocios != NULL){
+		if (ci == ciTSocio(lseSocios->socio)){
+			return lseSocios->socio;
+			}
+		lseSocios = lseSocios->sig;
+		}
     return NULL;
 }
 
 TSocio obtenerNesimoTLSESocios(TLSESocios lseSocios, int n){
+	if (lseSocios != NULL){
+		for (int i = 1; i < n; i++){
+			lseSocios = lseSocios->sig;
+		}
+		return lseSocios->socio;
+	}
 	return NULL;
 }
 
 nat cantidadTLSESocios(TLSESocios lseSocios){
+	nat cant = 0;
+	while (lseSocios != NULL){
+		cant++;
+		lseSocios = lseSocios->sig;
+	}
 	return 0;
 }
 
 void removerSocioTLSESocios(TLSESocios &lseSocios, int ci){
+	if (ci == ciTSocio(lseSocios->socio)){
+		TLSESocios aux =lseSocios;
+		lseSocios = lseSocios->sig;
+		liberarTSocio(aux->socio);
+		delete aux;
+	} else {
+		TLSESocios actual = lseSocios;
+		while (actual->sig != NULL && ci != ciTSocio(actual->sig->socio)){
+			actual = actual->sig;
+		}
+		if (actual->sig != NULL){
+			TLSESocios aux = actual->sig;
+			actual->sig = actual->sig->sig;
+			liberarTSocio(aux->socio);
+			delete aux;
+		}
+	}
 }
--- tarea2/src/prestamo.cpp
+++ tarea2/src/prestamo.cpp
@@ -2,41 +2,99 @@
 #include "../include/prestamo.h"
 
 struct rep_prestamo {
+  TSocio socio;
+  TLibro libro;
+  TFecha fechaRetiro;
+  TFecha fechaDevolucion;  
 };
 
 TPrestamo crearTPrestamo(TSocio socio, TLibro libro, TFecha fechaRetiro){
-  return NULL;
+  TPrestamo nuevoPrestamo = new rep_prestamo;
+  nuevoPrestamo->socio = socio;
+  nuevoPrestamo->libro = libro;
+  nuevoPrestamo->fechaRetiro = fechaRetiro;
+  nuevoPrestamo->fechaDevolucion = NULL;
+  return nuevoPrestamo;
 }
  
 void imprimirTPrestamo(TPrestamo prestamo){
+  printf("Préstamo de libro %s a %s %s.\n", tituloTLibro(prestamo->libro), nombreTSocio(prestamo->socio), apellidoTSocio(prestamo->socio));
+  imprimirTFecha(prestamo->fechaRetiro);
+  if (prestamo->fechaDevolucion != NULL) {
+    imprimirTFecha(prestamo->fechaDevolucion);
+  } else {
+    printf("No retornado\n");
+  }
 }
 
 void liberarTPrestamo(TPrestamo &prestamo){
+  if (prestamo != NULL){
+    liberarTSocio(prestamo->socio);
+    liberarTLibro(prestamo->libro);
+    liberarTFecha(prestamo->fechaRetiro);
+    liberarTFecha(prestamo->fechaDevolucion);
+    delete prestamo;
+    prestamo = NULL;
+  }
 }
 
 TSocio socioTPrestamo(TPrestamo prestamo){
+  if (prestamo != NULL) {
+    return prestamo->socio;
+  }
   return NULL;
 }
  
 TFecha fechaRetiroTPrestamo(TPrestamo prestamo){
+  if (prestamo != NULL) {
+    return prestamo->fechaRetiro;
+  }
   return NULL;
 }
 
 TFecha fechaDevolucionTPrestamo(TPrestamo prestamo){
+  if (prestamo != NULL) {
+    return prestamo->fechaDevolucion;
+  }
   return NULL;
 }
 
 TLibro libroTPrestamo(TPrestamo prestamo){
+  if (prestamo != NULL) {
+    return prestamo->libro;
+  }
   return NULL;
 }
 
 bool fueRetornadoTPrestamo(TPrestamo prestamo){
+  if (prestamo != NULL && prestamo->fechaDevolucion != NULL) {
+    return true;
+  }
   return false;
 }
 
 void actualizarFechaDevolucionTPrestamo(TPrestamo prestamo, TFecha fechaDevolucion){
+  if (prestamo != NULL) {
+    if (prestamo->fechaDevolucion != NULL){
+      liberarTFecha(prestamo->fechaDevolucion);
+    } 
+      prestamo->fechaDevolucion = fechaDevolucion;
+  }
 }
 
+
 TPrestamo copiarTPrestamo(TPrestamo prestamo){
+  if (prestamo != NULL) {
+    TPrestamo copia = new rep_prestamo;
+    copia->socio = prestamo->socio;
+    copia->libro = prestamo->libro;
+    copia->fechaRetiro = prestamo->fechaRetiro;
+    if (prestamo->fechaDevolucion != NULL) {
+      copia->fechaDevolucion = prestamo->fechaDevolucion;
+    } 
+    else {
+      copia->fechaDevolucion = NULL; 
+    }
   return NULL;
+  }
 }
